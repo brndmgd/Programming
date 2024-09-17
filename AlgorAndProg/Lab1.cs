9d2542e4c25f78eebf02a92664df04ebcc061a08
Egor <brndmgdwastaken@gmail.com>
2024-09-17 13:33:07 +0600

    Add files via upload

---
+++ AlgorAndProg/Lab1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Zadanie1
+{
+    static void Main()
+    {
+        /*Задание 1. Заменить значения двух переменных без использования третьей*/
+        /*int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        Console.WriteLine(a);
+        Console.WriteLine(b);*/
+        /*Задание 2. вывести минимум из двух переменных. */
+        /*int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int c = (a + b - Math.Abs(a - b)) / 2;
+        Console.WriteLine(c);*/
+        /*Задание 3. Найти путь для полива грядок по определённому алгоритму */
+        Console.WriteLine("Введите ширину грядки");
+        int l = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите длину грядки");
+        int m = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите расстояние от колодца до грядки");
+        int k = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите количество грядок");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int result = 2 * n * (k + m + l) + (n - 1) * n * l;
+        Console.WriteLine(result);
+    }
+}
---
+++ AlgorAndProg/Lab2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+class HelloWorld
+{
+    static void Main()
+    {
+        /* Дана последовательность элементов в количестве n не менее 3. Выполнить задачи:*/
+        /* Задача 1. Определить количество элеменов меньших нуля */
+        /*
+        int n = Convert.ToInt32(Console.ReadLine());
+        int k = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int readResult = Convert.ToInt32(Console.ReadLine());
+            if (readResult < 0)
+            {
+                k++;
+            }
+        }
+        */
+        /* Задача 2. Определить среди элементов второй максимальный элемент */
+        /*
+        int n = Convert.ToInt32(Console.ReadLine());
+        int firstMax = 0, secondMax = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int readResult = Convert.ToInt32(Console.ReadLine());
+            if (i == 0)
+            {
+                firstMax = readResult;
+            }
+            else if (readResult >= firstMax)
+            {
+                secondMax = firstMax;
+                firstMax = readResult;
+            }
+            else if (readResult > secondMax)
+            {
+                secondMax = readResult;
+            }
+        }
+        Console.WriteLine(secondMax);
+        */
+        /* Задача 3. Определить количество элементов являющиеся локальными минимумами */
+        /*
+        int n = Convert.ToInt32(Console.ReadLine());
+        int k = 0;
+        int prev1Result = 0, prev2Result = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int readResult = Convert.ToInt32(Console.ReadLine());
+            if (i >= 2)
+            {
+                if ((prev1Result < prev2Result) && (prev1Result < readResult))
+                {
+                        k++;
+                }
+            }
+            prev2Result = prev1Result;
+            prev1Result = readResult;
+        }
+        Console.WriteLine(k);
+        */
+    }
+}
