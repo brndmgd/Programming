eaa64bbf8596e6ca5df63fa04d9570d8c663b7ed
Egor <brndmgdwastaken@gmail.com>
2024-11-02 20:36:12 +0600

    Add files via upload

---
+++ AlgorAndProg/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Devil
+{
+    public static void Main()
+    {
+        int maxN = Convert.ToInt32(Console.ReadLine());
+        double c = 0;
+        int maxNc = maxN;
+        int twoC = 2;
+        while (maxNc > 0)
+        {
+            maxNc /= 2;
+            c += maxN / (twoC-1);
+            twoC *= 2;
+        }
+        Console.WriteLine(c);
+    }
+}
