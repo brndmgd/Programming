1949b29f3712317391a6f1b94cec25854cf99f25
Egor <brndmgdwastaken@gmail.com>
2024-11-02 20:38:22 +0600

    Update and rename Program.cs to Devil.cs

--- AlgorAndProg/Devil.cs
+++ AlgorAndProg/Devil.cs
@@ -7,12 +7,12 @@ class Devil
         int maxN = Convert.ToInt32(Console.ReadLine());
         double c = 0;
         int maxNc = maxN;
-        int twoC = 2;
+        int twoP = 2;
         while (maxNc > 0)
         {
             maxNc /= 2;
-            c += maxN / (twoC-1);
-            twoC *= 2;
+            c += maxN / (twoP-1);
+            twoP *= 2;
         }
         Console.WriteLine(c);
     }
