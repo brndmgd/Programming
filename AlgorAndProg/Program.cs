7cdfac84e6d21c81f7734b678f2fa7f798de3e6b
Egor <brndmgdwastaken@gmail.com>
2024-12-14 19:05:08 +0600

    Add files via upload

---
+++ AlgorAndProg/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Soldiers
+{
+    static void Main()
+    {
+        Console.Write("Введите количество солдат: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int i = 1;
+        while (i * 2 < n)
+        {
+            i *= 2;
+        }
+        int a;
+        if (n - i <= i / 2) a = n - i;
+        else a = i*2 - n;
+        Console.WriteLine("Количество групп по три: " + a);
+    }
+}
