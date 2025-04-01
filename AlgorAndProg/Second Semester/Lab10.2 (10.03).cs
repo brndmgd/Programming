40bfae3146b43ee369eb4b5fbb7bef34d16d4862
Egor <brndmgdwastaken@gmail.com>
2025-04-01 17:40:12 +0600

    Add files via upload

---
+++ AlgorAndProg/Second Semester/Lab10.2 (10.03).cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+
+class TwoElements
+{
+    public double A { get; set; }
+    public double B { get; set; }
+
+    public TwoElements(double a, double b)
+    {
+        A = a;
+        B = b;
+    }
+
+    public double Summ()
+    {
+        A = A + B;
+        return A;
+    }
+
+    public double Diff()
+    {
+        A = A - B;
+        return A;
+    }
+
+    public double Mult()
+    {
+        A = A * B;
+        return A;
+    }
+
+    public double Frac()
+    {
+        if (B != 0) A = A / B;
+        else A = double.NaN;
+        return A;
+    }
+}
+
+delegate double First();
+delegate double Second();
+
+class Program
+{
+    static void Main()
+    {
+        double a = Convert.ToInt32(Console.ReadLine());
+        double b = Convert.ToInt32(Console.ReadLine());
+        TwoElements obj = new TwoElements(a, b);
+        First operation1 = (First)obj.Summ + obj.Diff + obj.Mult;
+        Console.WriteLine(operation1());
+        obj = new TwoElements(a, b);
+        Second operation2 = (Second)obj.Mult + obj.Summ + obj.Frac;
+        Console.WriteLine(operation2());
+    }
+}
---
+++ AlgorAndProg/Second Semester/Lab5.1 (10.03).cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Phone
+{
+    string Number;
+    public string Provider { get; }
+
+    public Phone(string number, string provider)
+    {
+        Number = number;
+        Provider = provider;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Phone> phones = new List<Phone>();
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input == "0") 
+                break;
+
+            string[] arInput = input.Split();
+            Phone curPhone = new Phone(arInput[0], arInput[1]);
+            phones.Add(curPhone);
+        }
+
+        Dictionary<string, int> providers = new Dictionary<string, int>();
+        foreach (var i in phones)
+        {
+            if (providers.Keys.Contains(i.Provider))
+                providers[i.Provider]++;
+
+            else
+                providers[i.Provider] = 1;
+        }
+
+        int maxProvider = providers.Values.Max();
+        foreach (var i in providers)
+        {
+            if (i.Value == maxProvider)
+                Console.WriteLine(i.Key);
+        }
+    }
+}
