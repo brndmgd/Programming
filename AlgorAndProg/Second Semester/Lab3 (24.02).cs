65c4073289658fa1f7b04f1cd99e352ea0ba4042
Egor <brndmgdwastaken@gmail.com>
2025-04-01 17:28:38 +0600

    Add files via upload

---
+++ AlgorAndProg/Second Semester/Lab3 (24.02).cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string brackets = "(){}[]";
+        string input = Console.ReadLine();
+        bool isWrong = false;
+        Stack<char> brSequence = new Stack<char>();
+
+        foreach (var i in input)
+        {
+            if (brackets.IndexOf(i) % 2 == 0)
+            {
+                brSequence.Push(i);
+            }
+            else if (brackets.Contains(i) && brackets.IndexOf(i) % 2 != 0)
+            {
+                bool notEmpty = brSequence.TryPop(out char curBr);
+                int difference = brackets.IndexOf(i) - brackets.IndexOf(curBr);
+                if (!notEmpty || difference != 1)
+                {
+                    isWrong = true;
+                    break;
+                }
+            }
+        }
+
+        if (isWrong)
+        {
+            Console.WriteLine("Последовательность неверна");
+        }
+        else
+        {
+            Console.WriteLine("Последовательность верна");
+        }
+    }
+}
