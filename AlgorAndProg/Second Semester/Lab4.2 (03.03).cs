838596500b7ed8c276d5737be1d74819cde471b5
Egor <brndmgdwastaken@gmail.com>
2025-04-01 17:35:04 +0600

    Add files via upload

---
+++ AlgorAndProg/Second Semester/Lab4.2 (03.03).cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine().Trim();
+        string[] inList = input.Split();
+        HashSet<string> unique = inList.ToHashSet();
+        Dictionary<string, int> quantity = new Dictionary<string, int>();
+        foreach (var i in inList)
+        {
+            if (!quantity.Keys.Contains(i))
+            {
+                quantity[i] = 1;
+            }
+            else
+            {
+                quantity[i]++;
+            }
+        }
+        
+        foreach (var i in unique)
+        {
+            Console.Write($"{i} ");
+        }
+        Console.WriteLine();
+        foreach (var i in quantity.Keys)
+        {
+            Console.Write($"\"{i}\":{quantity[i]} ");
+        }
+    }
+}
