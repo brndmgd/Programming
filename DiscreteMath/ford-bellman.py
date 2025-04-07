b467ab9e4e854c150fb44920855f796ba352ad96
Egor <brndmgdwastaken@gmail.com>
2025-04-07 12:58:11 +0600

    Update ford-bellman.py

--- DiscreteMath/ford-bellman.py
+++ DiscreteMath/ford-bellman.py
@@ -13,7 +13,7 @@ for k in range(1, len(weight_matrix)):
     for i in range(len(weight_matrix)):
         for j in range(len(weight_matrix)):
             if lengths[i] == 'i':
-                lengths[i] = weight_matrix[j][i] if weight_matrix[j][i] != 0 else 'i'
+                lengths[i] = weight_matrix[j][i] + lengths[j] if weight_matrix[j][i] != 0 else 'i'
             else:
                 if lengths[j] != 'i' and weight_matrix[j][i] != 0:
                     lengths[i] = min(weight_matrix[j][i] + lengths[j], lengths[i])
